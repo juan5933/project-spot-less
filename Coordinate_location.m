a353aaa9cd500e1450b46710131bbe8eaee9f645
Alsexy11 <alexandersyrros@gmail.com>
2024-10-15 00:29:01 +1100

    Absolute locations
    Absolute location of significant events,

---
+++ Coordinate_location.m
@@ -0,0 +1,20 @@
+% UR3e and UR16 robot locations
+UR3e = [-0.25, 0.6, 0.65];
+UR16 = [-0.25, 0.2, 0.65];
+
+% Initial Plate Locations
+Initial_Plate_Locations = [
+    0.8, 0.2, 0.65;  % Plate 1
+    0.8, 0.3, 0.65;  % Plate 2
+    0.8, 0.4, 0.65   % Plate 3
+];
+
+% Final Plate Locations
+Final_Plate_Locations = [
+    0.25, 0.2, 0.65;  % Plate 1
+    0.25, 0.3, 0.65;  % Plate 2
+    0.25, 0.4, 0.65   % Plate 3
+];
+
+% Cleaning/Wiping Coordinate
+Cleaning_Wiping_Coordinate = [-0.25, 0.4, 0.8];
