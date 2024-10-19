4a4d3ef13991e856ee1e61e345bfb82023eb782b
juan5933 <157328481+juan5933@users.noreply.github.com>
2024-10-19 21:31:17 +1100

    Update Plates.m

--- Plates.m
+++ Plates.m
@@ -1,32 +1,32 @@
 classdef Plates < handle
     % Class to manage the placement and attributes of bricks in the environment
-    
+
     properties
         platesStart  % Coordinates for the starting positions of the bricks
         platesEnd    % Coordinates for the destination (end) positions of the bricks
         plates_h     % Handles to the graphical brick objects in the environment
     end
-    
+
     methods
         function self = Plates()
             % Constructor: Initialize the brick starting and ending positions
             % brickStart: 3D coordinates for where the bricks are initially placed
             % brickEnd: 3D coordinates for where the bricks will be moved to
-            
-            self.platesStart = [-1.55, 0.6  1.425; 
-                                -1.55, 0.6, 1.4;  
-                                -1.55, 0.6, 1.375;                
+
+            self.platesStart = [-1.55, 0.6  1.4; 
+                                -1.55, 0.6, 1.375;
+                                -1.55, 0.6, 1.35;
                                 ];  % Plate positions
-            
-            self.platesEnd = [  0, 1, 1.25;
-                                0, 1, 1.30;
-                                0, 1, 1.32;  
+
+            self.platesEnd = [  0.05, 0.603, 1.4;
+                                0.05, 0.603, 1.375;
+                                0.05, 0.603, 1.35;
                                      ];  % Corresponding end positions for each brick
-            
+
             % Preallocate the cell array for storing the handles to the bricks in the environment
-            self.plates_h = cell(1, 9);  % Array to store brick handles (9 bricks)
+            self.plates_h = cell(1, 3);  % Array to store brick handles (9 bricks)
         end
-        
+
         function PlacePlates(self)
             % Method to place the bricks at their start positions in the environment
 
@@ -37,6 +37,6 @@ classdef Plates < handle
             end
         end
 
-    
+
     end
 end
