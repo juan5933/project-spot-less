0a64ceda1f94a463165f4935405e92e1ef904652
juan5933 <157328481+juan5933@users.noreply.github.com>
2024-10-30 06:01:56 +1100

    Update EnvironmentSetup.m

--- EnvironmentSetup.m
+++ EnvironmentSetup.m
@@ -1,3 +1,65 @@
+% classdef EnvironmentSetup < handle
+%     properties
+%         objectVertices = {};  % Cell array to store vertices of each object
+%     end
+% 
+%     methods
+%         function self = EnvironmentSetup()
+%             % Constructor: setup the environment and add objects
+%             self.SetupWorkspace();
+%             self.AddJPG();
+%             self.ObjectLayout();  % Call ObjectLayout to add all PLY objects
+%         end
+% 
+%         function SetupWorkspace(self)
+%             % Setup the workspace with defined view and axis settings
+%             clf;
+%             axis([-6 6 -3 3 -0.5 2]);  % Set axis limits for the workspace
+%             axis manual;               % Prevents automatic axis resizing
+%             view(3);                   % Set a 3D view
+%             axis equal;                % Ensure equal scaling on all axes
+%             hold on;                   % Retain the current plot when adding new objects
+%         end
+% 
+%         function AddJPG(self)
+%             % Add concrete texture to the environment
+%             surf([-3,-3;3,3], [-2.7,4;-2.7,4], [0,0;0,0], ...
+%                 'CData', imread('concrete.jpg'), 'FaceColor', 'texturemap');
+%             hold on;
+%         end
+% 
+%         function ObjectLayout(self)
+%             % Layout all the objects (fences, tables, etc.) in the environment
+%             self.AddPly('emergencyStopButton.ply', [2, 2, -0.45], trotx(-pi/2), 0.5);
+%             self.AddPly('emergencyStopButton.ply', [2.5, 2, -0.45], trotx(-pi/2), 0.5);
+%             self.AddPly('personMaleCasual.ply', [-1,-3,0], trotz(pi), 1.2);
+%             self.AddPly('fireExtinguisher.ply', [2.1, -1, 0], trotz(pi), 1);
+%             self.AddPly('kitchenenvironment.ply', [0,0,0], trotz(pi)*1.45, 200);
+%             self.AddPly('PlateStand.ply', [1.451,-0.35, 1.31], trotz(pi), 1);
+%             self.AddPly('PlateStand.ply', [-0.05, -0.35, 1.31], trotz(pi), 1);
+%         end
+% 
+%         function AddPly(self, fileName, position, rotationFactor, scaleFactor)
+%             % Add PLY object to the environment
+%             ply_h = PlaceObject(fileName, position);  % Place the object in the scene
+%             verts = [get(ply_h, 'Vertices'), ones(size(get(ply_h, 'Vertices'), 1), 1)] * rotationFactor;  % Rotate the object
+%             verts(:, 1:3) = verts(:, 1:3) * scaleFactor;  % Scale the object
+%             set(ply_h, 'Vertices', verts(:, 1:3));  % Update the vertices of the object
+% 
+%             % Store the transformed vertices for collision detection
+%             self.objectVertices{end+1} = verts(:, 1:3);
+%             hold on;
+%         end
+% 
+%         function allVertices = GetEnvironmentVertices(self)
+%             % Collect all vertices from objects for collision detection
+%             allVertices = vertcat(self.objectVertices{:});
+%         end
+%     end
+% end
+
+
+
 classdef EnvironmentSetup
 
     methods
@@ -28,12 +90,12 @@ classdef EnvironmentSetup
         function ObjectLayout(self)
             % Layout all the objects (fences, tables, etc.) in the environment
             self.AddPly('emergencyStopButton.ply', [2, 2, -0.45], trotx(-pi/2), 0.5);
-            self.AddPly('emergencyStopButton.ply', [2.5, 2, -0.45], trotx(-pi/2), 0.5);
+            self.AddPly('emergencyStopButton.ply', [2, -2, 3.9], trotx(pi/2), 0.5);
             self.AddPly('personMaleCasual.ply', [-1,-3,0], trotz(pi), 1.2);
             self.AddPly('fireExtinguisher.ply', [2.1, -1, 0], trotz(pi), 1);
             self.AddPly('kitchenenvironment.ply', [0,0,0], trotz(pi)*1.45, 200);
-            self.AddPly('PlateStand.ply', [1.451,-0.35, 1.31], trotz(pi), 1);
-            self.AddPly('PlateStand.ply', [-0.05, -0.35, 1.31], trotz(pi), 1);
+            self.AddPly('PlateStand.ply', [1.451,-0.31, 1.31], trotz(pi), 1);
+            self.AddPly('PlateStand.ply', [-0.15, -0.35, 1.31], trotz(pi), 1);
         end
         
         function AddPly(self, fileName, position, rotationFactor, scaleFactor)
