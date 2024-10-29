b58cecbd66b2a8dd60435adfe5dd620037d7f390
juan5933 <157328481+juan5933@users.noreply.github.com>
2024-10-30 06:03:25 +1100

    Update UR16e.m

--- UR16e.m
+++ UR16e.m
@@ -1,3 +1,4 @@
+
 classdef UR16e < RobotBaseClass
     %% UR16e Universal Robot 3kg payload robot model
     %
@@ -17,7 +18,7 @@ classdef UR16e < RobotBaseClass
                 if nargin == 2
                     error('If you set useTool you must pass in the toolFilename as well');
                 elseif nargin == 0 % Nothing passed
-                    baseTr = transl(-0.5,0.35,1.3)* rpy2tr(0, 0, 180, 'deg');  
+                    baseTr = transl(-0.55,0.35,1.3)* rpy2tr(0, 0, 180, 'deg');  
                 end             
             else % All passed in 
                 self.useTool = useTool;
@@ -28,53 +29,54 @@ classdef UR16e < RobotBaseClass
           
             self.CreateModel();
 
-            % Create ellipsoid for collision detection
+            % % Create ellipsoid for collision detection
+            % 
+            % % Define the center points and radii for each link
+            % % Example values, customize as needed for each link
+            % centerPoints = [
+            %     0, 0, 0;  % Link 1
+            %     0, 0, 0;  % Link 2
+            %     0.24, 0, 0.16;  % Link 3
+            %     0.2, 0, 0.03;  % Link 4
+            %     0, 0, 0;  % Link 5
+            %     0, 0, 0;  % Link 6
+            %     0, 0, 0   % Link 7
+            %     ];
+            % 
+            % radiiValues = [
+            %     0.12, 0.12, 0.12;  % Link 1
+            %     0.13, 0.10, 0.13; % Link 2
+            %     0.38, 0.13, 0.13;  % Link 3
+            %     0.3, 0.12, 0.12; % Link 4
+            % 
+            %     0.1, 0.1, 0.1;  % Link 5
+            %     0.08, 0.08, 0.08;   % Link 6
+            %     0.08, 0.08, 0.05   % Link 7
+            %     ];
+            % 
+            % % Generate an ellipsoid for each link with specified parameters
+            % for i = 1:7
+            %     centerPoint = centerPoints(i, :);
+            %     radii = radiiValues(i, :);
+            % 
+            %     % Generate ellipsoid with custom parameters for this link
+            %     [X, Y, Z] = ellipsoid(centerPoint(1), centerPoint(2), centerPoint(3), radii(1), radii(2), radii(3));
+            % 
+            %     % Store points and faces for the model
+            %     self.model.points{i} = [X(:), Y(:), Z(:)];
+            % 
+            %     % Suppress warnings for Delaunay triangulation and create faces
+            %     warning off
+            %     self.model.faces{i} = delaunay(self.model.points{i});
+            %     warning on
+            % end
+            % 
+            % self.model.plot3d([0,0,0,0,0,0]);
 
-            % Define the center points and radii for each link
-            % Example values, customize as needed for each link
-            centerPoints = [
-                0, 0, 0;  % Link 1
-                0, 0, 0;  % Link 2
-                0.12, 0, 0.12;  % Link 3
-                0.12, 0, 0;  % Link 4
-                0, 0, 0;  % Link 5
-                0, 0, 0;  % Link 6
-                0, 0, 0   % Link 7
-                ];
-
-            radiiValues = [
-                0.08, 0.08, 0.15;  % Link 1
-                0.07, 0.07, 0.08; % Link 2
-                0.19, 0.09, 0.09;  % Link 3
-                0.18, 0.08, 0.08; % Link 4
-                0.05, 0.05, 0.05;  % Link 5
-                0.05, 0.05, 0.05;   % Link 6
-                0.05, 0.05, 0.05   % Link 7
-                ];
-
-            % Generate an ellipsoid for each link with specified parameters
-            for i = 1:7
-                centerPoint = centerPoints(i, :);
-                radii = radiiValues(i, :);
-
-                % Generate ellipsoid with custom parameters for this link
-                [X, Y, Z] = ellipsoid(centerPoint(1), centerPoint(2), centerPoint(3), radii(1), radii(2), radii(3));
-
-                % Store points and faces for the model
-                self.model.points{i} = [X(:), Y(:), Z(:)];
-
-                % Suppress warnings for Delaunay triangulation and create faces
-                warning off
-                self.model.faces{i} = delaunay(self.model.points{i});
-                warning on
-            end
 
 
 
-            % axis equal
             axis([-0.1 0.1 -0.1 0.1 -0.1 0.1]); % Adjust based on your ellipsoid dimensions
-
-            self.model.plot3d([0,0,0,0,0,0]);
             camlight
             hold on
 
