32e92d48e5423b857c4778892eb8f991500ef61c
AshwenKugan <Ashwen.Kugan@student.uts.edu.au>
2024-11-08 18:41:43 +1100

    Update EnvironmentSetup.m

--- EnvironmentSetup.m
+++ EnvironmentSetup.m
@@ -1,50 +1,50 @@
 classdef EnvironmentSetup
+    % Class to set up a 3D environment and add objects for visualization
 
     methods
         function self = EnvironmentSetup()
-            % Constructor: setup the environment and add objects
-            self.SetupWorkspace();
-            self.AddJPG();
-            self.ObjectLayout();  % Call ObjectLayout to add all PLY objects
+            % Constructor: Initialize and configure the environment
+            self.SetupWorkspace();  % Setup the 3D workspace settings
+            self.AddJPG();          % Add a background texture (e.g., concrete)
+            self.ObjectLayout();    % Arrange predefined PLY objects in the scene
         end
 
         function SetupWorkspace(self)
-            % Setup the workspace with defined view and axis settings
-            % clf;
-            axis([-6 6 -3 3 -0.5 2]);  % Set axis limits for the workspace
-            axis manual;               % Prevents automatic axis resizing
-            view(3);                   % Set a 3D view
-            axis equal;                % Ensure equal scaling on all axes
-            hold on;                   % Retain the current plot when adding new objects
+            % Configure the 3D workspace view and axis limits
+            axis([-6 6 -3 3 -0.5 2]);  % Set the boundaries for the 3D plot
+            axis manual;              % Fix the axis limits to avoid auto-scaling
+            view(3);                  % Set the view to 3D perspective
+            axis equal;               % Ensure uniform scaling for all axes
+            hold on;                  % Retain current plot when adding objects
         end
 
         function AddJPG(self)
-            % Add concrete texture to the environment
-            surf([-2.4,-2.4;3,3], [-2.7,4;-2.7,4], [0,0;0,0], ...
-                'CData', imread('concrete.jpg'), 'FaceColor', 'texturemap');
-            hold on;
+            % Render a texture as the environment background
+            surf([-2.4, -2.4; 3, 3], [-2.7, 4; -2.7, 4], [0, 0; 0, 0], ...
+                'CData', imread('concrete.jpg'), 'FaceColor', 'texturemap');  % Apply texture
+            hold on;  % Maintain current plot
         end
     
         function ObjectLayout(self)
-            % Layout all the objects (fences, tables, etc.) in the environment
+            % Add multiple objects (e.g., buttons, models) to the environment
             self.AddPly('emergencyStopButton.ply', [2, 2, -0.45], trotx(-pi/2), 0.5);
             self.AddPly('emergencyStopButton.ply', [2, -2, 3.9], trotx(pi/2), 0.5);
-            self.AddPly('personMaleCasual.ply', [-1,-3,0], trotz(pi), 1.2);
+            self.AddPly('personMaleCasual.ply', [-1, -3, 0], trotz(pi), 1.2);
             self.AddPly('fireExtinguisher.ply', [2.1, -1, 0], trotz(pi), 1);
-            self.AddPly('kitchenenvironment.ply', [0,0,0], trotz(pi)*1.45, 200);
-            self.AddPly('PlateStand.ply', [1.451,-0.31, 1.31], trotz(pi), 1);
+            self.AddPly('kitchenenvironment.ply', [0, 0, 0], trotz(pi) * 1.45, 200);
+            self.AddPly('PlateStand.ply', [1.451, -0.31, 1.31], trotz(pi), 1);
             self.AddPly('PlateStand.ply', [-0.15, -0.35, 1.31], trotz(pi), 1);
             self.AddPly('slipMat.ply', [0.2, 0.22, 0], trotz(-pi), 4);
             self.AddPly('slipMat.ply', [-0.08, 0.22, 0], trotz(-pi), 4);
         end
         
         function AddPly(self, fileName, position, rotationFactor, scaleFactor)
-            % Add PLY object to the environment
-            ply_h = PlaceObject(fileName, position);  % Place the object in the scene
-            verts = [get(ply_h, 'Vertices'), ones(size(get(ply_h, 'Vertices'), 1), 1)] * rotationFactor;  % Rotate the object
-            verts(:, 1:3) = verts(:, 1:3) * scaleFactor;  % Scale the object
-            set(ply_h, 'Vertices', verts(:, 1:3));  % Update the vertices of the object
-            hold on;
+            % Load and position a PLY object with rotation and scaling
+            ply_h = PlaceObject(fileName, position);  % Insert the PLY object at the specified position
+            verts = [get(ply_h, 'Vertices'), ones(size(get(ply_h, 'Vertices'), 1), 1)] * rotationFactor;  % Apply rotation
+            verts(:, 1:3) = verts(:, 1:3) * scaleFactor;  % Scale the vertices
+            set(ply_h, 'Vertices', verts(:, 1:3));  % Update the object vertices after transformation
+            hold on;  % Keep the plot active for further additions
         end
     end
 end
